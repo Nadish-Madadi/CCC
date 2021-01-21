f824768f0a2131a3ff70e6d9cd234ee848628ced
Nadish-Madadi <75193860+Nadish-Madadi@users.noreply.github.com>
2021-01-21 09:45:00 -0500

    Create CCC J1 2009 - C++.cpp

---
+++ CCC J1 2009 - C++.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int num1;
+	int num2;
+	int num3;
+
+	cout << "Digit 11?";
+	cin >> num1;
+	cout << "Digit 12?";
+	cin >> num2;
+	cout << "Digit 13?";
+	cin >> num3;
+
+	cout << "The 1-3-sum is " << 91 + num1 * 1 + num2 * 3 + num3 * 1;
+
+
+	return 0;
+
+}
